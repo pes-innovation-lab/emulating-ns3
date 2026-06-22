53e7f433ff9615bddb86d935ee60ecd276a96883
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2026-06-22 19:25:16 +0530

    fix: improve error handling in install_plugin.sh and use `sudo` consistently

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,17 +1,20 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-cd ./plugin || (echo "missing plugin dir" && exit)
+cd ./plugin || {
+    echo "missing plugin dir" >&2
+    exit 1
+}
 
 # steps taken from https://docs.docker.com/engine/extend/
-docker build -t rootfsimage .
-id=$(docker create rootfsimage true)
+sudo docker build -t rootfsimage .
+id=$(sudo docker create rootfsimage true)
 if [ -d "../plugin-data/rootfs" ]; then
     sudo rm -rf ../plugin-data/rootfs
 fi
 mkdir -p ../plugin-data/rootfs
 sudo docker export "$id" | sudo tar -x -C ../plugin-data/rootfs
-docker rm -vf "$id"
-docker rmi rootfsimage
+sudo docker rm -vf "$id"
+sudo docker rmi rootfsimage
 
 cd ..
 sudo docker plugin create netkit-ns-3 ./plugin-data
